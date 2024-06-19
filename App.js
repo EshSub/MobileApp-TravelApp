cb15b34d5036ec6c7bf8376547fff791059c4256
kamadi2000 <kmaliyanage@gmail.com>
2024-06-19 17:18:10 +0530

    glue stack UI added

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+import { config } from '@gluestack-ui/config';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <GluestackUIProvider config={config}>
+        <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </GluestackUIProvider>
   );
 }
 
