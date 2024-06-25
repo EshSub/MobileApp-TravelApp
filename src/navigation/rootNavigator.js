53fb747997b14e657afa746cba32cdd576672d0b
kamadi2000 <kmaliyanage@gmail.com>
2024-06-25 20:37:28 +0530

    add user login

---
+++ src/navigation/rootNavigator.js
@@ -0,0 +1,45 @@
+import { Text, View } from "react-native";
+import Login from "../views/login";
+import SignUp from "../views/signUp";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useDispatch, useSelector } from "react-redux";
+import { login, selectUser } from "../redux/slices/userSlice";
+import { Button, ButtonText } from "@gluestack-ui/themed";
+
+const Stack = createNativeStackNavigator();
+
+export default function RootNavigator() {
+  const user = useSelector(selectUser);
+
+  return (
+    <Stack.Navigator>
+      {user ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+}
+
+function HomeScreen() {
+    const dispatch = useDispatch()
+    const handleLogout = () => {
+        dispatch(login(null))
+    }
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Button onPress={handleLogout}>
+        <ButtonText>
+            Logout
+        </ButtonText>
+      </Button>
+    </View>
+  );
+}
---
+++ src/views/login.js
@@ -0,0 +1,95 @@
+import { Button, ButtonText, EyeIcon, EyeOffIcon, FormControl, Heading, Input, InputField, InputIcon, InputSlot, Link, LinkText, Text, VStack } from "@gluestack-ui/themed";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import { useState } from "react";
+import { BACKEND_URL } from "../constants/utils";
+import { useDispatch } from "react-redux";
+import { login } from "../redux/slices/userSlice";
+
+function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+    const [username, setUsername] = useState()
+    const [password, setPassword] = useState()
+    const navigation = useNavigation()
+    const dispatch = useDispatch() 
+
+    const handleState = () => {
+      setShowPassword((showState) => {
+        return !showState;
+      });
+    };
+
+    const handleClick = async () => {
+      try {
+        const response = await axios.post(`${BACKEND_URL}/api/token/`,{ username : username, password : password })
+        if (response){
+          console.log(response.data)
+          // navigation.navigate("Home")
+          dispatch(login({
+            name: username,
+            loggedIn: true,
+            accessToken: response.data.access
+          }))
+        }
+      } catch (error) {
+        console.log('error', error)
+      }
+    }
+    return (
+      <FormControl
+        w='90%'
+        p='$4'
+        borderWidth='$1'
+        borderRadius='$lg'
+        borderColor='$borderLight300'
+        $dark-borderWidth='$1' $dark-borderRadius='$lg' $dark-borderColor='$borderDark800'
+      >
+        <VStack space='xl'>
+          <Heading color='$text900' lineHeight='$md'>
+            Login
+          </Heading>
+          <VStack space='xs'>
+            <Text color='$text500' lineHeight='$xs'>
+              Email
+            </Text>
+            <Input>
+              <InputField
+                type="text"
+                value={username}
+                onChangeText={(text) => setUsername(text)}
+              />
+            </Input>
+          </VStack>
+          <VStack space='xs'>
+            <Text color='$text500' lineHeight='$xs'>
+              Password
+            </Text>
+            <Input textAlign='center'>
+              <InputField
+                value={password}
+                type={showPassword ? 'text' : 'password'}
+                onChangeText={(text) => setPassword(text)}
+              />
+              <InputSlot pr='$3' onPress={handleState}>
+                {/* EyeIcon, EyeOffIcon are both imported from 'lucide-react-native' */}
+                <InputIcon as={showPassword ? EyeIcon : EyeOffIcon}  color='$darkBlue500'/>
+              </InputSlot>
+            </Input>
+          </VStack>
+          <Button
+            ml='auto'
+            onPress={handleClick}
+          >
+            <ButtonText color='$white'>
+              Login
+            </ButtonText>
+          </Button>
+          <Link onPress={() => navigation.navigate('SignUp')}>
+          <LinkText>Don't have an account ? Sign up</LinkText>
+          </Link>
+        </VStack>
+      </FormControl>
+    );
+  }
+
+export default Login;
